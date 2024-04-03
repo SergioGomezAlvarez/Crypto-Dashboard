add60b0c8d0cac2092adc362306e824087dc212b
SergioGomezAlvarez <sgmichielr@gmail.com>
2024-04-03 11:56:33 +0200

    Aligned trending coins with good logo's added a data chart but no data yet. added new api with bitcoin history

--- src/Staafdiagram.jsx
+++ src/Staafdiagram.jsx
@@ -4,8 +4,7 @@ import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBitcoin } from '@fortawesome/free-brands-svg-icons'
 import { faEthereum } from '@fortawesome/free-brands-svg-icons'
-
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 
 const Staafdiagram = ({ }) => {
@@ -14,6 +13,7 @@ const Staafdiagram = ({ }) => {
     const [bitc, setBitc] = useState();
     const [tether, setTeth] = useState();
     const [binance, setBinance] = useState();
+    const [solana, setSolana] = useState();
     const [coins, setCoins] = useState([]);
     const [example, setExample] = useState(
         {
@@ -22,22 +22,41 @@ const Staafdiagram = ({ }) => {
         }
 
     );
+    const [historicalData, setHistoricalData] = useState([]);
+
+    // const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }, { name: 'Page A', uv: 100, pv: 2400, amt: 2400 }, { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },];
+
+
 
     useEffect(() => {
         axios.get('https://api.coincap.io/v2/assets').then(function (response) {
             setCoins(response.data.data)
             const bitcoin = response.data.data.find(coins => coins.name === "Bitcoin")
             setBitc(bitcoin)
-            const ethereum = response.data.data.find(coins => coins.symbol === "ETH"); // Zoek Ethereum op basis van het symbool
+            const ethereum = response.data.data.find(coins => coins.symbol === "ETH");
             setCrypto(ethereum);
             console.log(response.data.data)
             const tether = response.data.data.find(coins => coins.name === "Tether");
             setTeth(tether);
             const binance = response.data.data.find(coins => coins.name === "BNB");
             setBinance(binance);
+            const solana = response.data.data.find(coins => coins.name === "Solana")
+            setSolana(solana);
         });
     }, []);
 
+    useEffect(() => {
+        axios.get('https://api.coincap.io/v2/assets/bitcoin/history?interval=d1')
+            .then(response => {
+                const data = response.data.data.slice(6).reverse(); // Dit zal de eerste 6 items uit de array verwijderen en de volgorde van de resterende items omkeren
+                setHistoricalData(data);
+            })
+            .catch(error => {
+                console.error('Error fetching historical data:', error);
+            });
+    }, []);
+
+    
     return (
         <>
             <div className="container-main">
@@ -52,25 +71,29 @@ const Staafdiagram = ({ }) => {
                                 <h1 className="price-btc-text">
                                     <img className="eth-logo" src="images/ethereum-logo-big.png"></img> : {!crypto ? null : Number(crypto.priceUsd).toFixed(2)}
                                 </h1>
-                                <div className="price-tether-text">
+                                <h1 className="price-btc-text">
                                     <img className="tether-logo" src="images/tether-logo-big.png"></img> : {!tether ? null : Number(tether.priceUsd).toFixed(2)}
-                                </div>
-                                <div className="price-binance-text">
+                                </h1>
+                                <h1 className="price-btc-text">
                                     <img className="binance-logo" src="images/binance-logo-big.png"></img> : {!binance ? null : Number(binance.priceUsd).toFixed(2)}
-                                </div>
+                                </h1>
+                                <h1 className="price-btc-text">
+                                    <img className="solana-logo" src="images/solana-logo-big.png"></img> : {!solana ? null : Number(solana.priceUsd).toFixed(2)}
+                                </h1>
                                 {/* {coins && coins.map((item, index) => {
                                        return   <div><FontAwesomeIcon icon={faBitcoin} size="xl" /> {item.id}</div>
                                 })} */}
                             </div>
                         </div>
-                        <div className="info-block-large">
-                            <div className="block-info-container">
-                                <h1 className="block-info-text">Burn</h1>
-                            </div>
-                        </div>
-                        <div className="info-block-large">
-                            <div className="block-info-container">
-                                <h1 className="block-info-text">Expenses</h1>
+                        <div className="info-block-large-graph">
+                            <div className="block-info-container-graph">
+                                <h1 className="block-info-text">Graph</h1>
+                                <LineChart width={750} height={250} data={[bitc].filter(Boolean)}>
+                                    <Line type="monotone" dataKey="priceUsd" stroke="#0059ff    " />
+                                    <CartesianGrid stroke="#ccc" />
+                                    <XAxis dataKey="name" />
+                                    <YAxis />
+                                </LineChart>
                             </div>
                         </div>
 
